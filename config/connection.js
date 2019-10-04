fc998b67e294af9082da6c076b29e944d0adbdd7
Mark0552 <50115357+Mark0552@users.noreply.github.com>
2019-10-04 02:24:38 -0400

    burger-app

--- config/connection.js
+++ config/connection.js
@@ -2,17 +2,17 @@ var mysql = require("mysql");
 var connection;
 
 
-if (process.env.JAWSDB_URL){
+if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
-  } else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3305,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-  });
-  }
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3305,
+        user: "root",
+        password: "password",
+        database: "burgers_db"
+    });
+}
 
 connection.connect(function (err) {
     if (err) {
